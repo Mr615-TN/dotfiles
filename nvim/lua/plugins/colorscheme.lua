7e7bf0abfbd2a18bb9cbaf70ad1d087892382ca9
Tanish Neema <tneema501@gmail.com>
2024-11-25 16:29:28 -0600

    pushing stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
-    -- add doom-one
-    {
-        "NTBBloodbath/doom-one.nvim",
-    },
+  -- add doom-one
+  {
+    "NTBBloodbath/doom-one.nvim",
+  },
 
-    -- Configure LazyVim to load doom-one
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "doom-one",
-        },
+  -- Configure LazyVim to load doom-one
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "doom-one",
     },
+  },
 }
