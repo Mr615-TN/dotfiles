740673d74646eeb0dfbd1de8c81c2092bdedc366
Mr615-TN <tneema501@gmail.com>
2024-12-12 21:13:59 -0600

    updated neovim to remove codeium

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -7,14 +7,4 @@ return {
       table.insert(opts.sources, { name = "emoji" })
     end,
   },
-  {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({})
-    end,
-  },
 }
