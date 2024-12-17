62c65f38822d049940af1d683fa404e361fa5927
Mr615-TN <tneema501@gmail.com>
2024-12-16 18:49:40 -0600

    updated blink.cmp so that I can press enter and autocomplete

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -6,7 +6,7 @@ return {
     dependencies = "rafamadriz/friendly-snippets",
     build = "cargo build --release",
     opts = {
-      keymap = { preset = "default" },
+      keymap = { preset = "enter" },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
