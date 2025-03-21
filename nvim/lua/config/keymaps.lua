afe8cb9fcaac71084b4c27407081d5253c885e48
Tanish Neema <tneema501@gmail.com>
2025-03-20 21:11:18 -0500

    Update: keymaps.lua and options.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,54 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Define keymap function for convenience
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Set leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- === General Keybindings ===
+map("i", "jj", "<Esc>", opts) -- Exit insert mode quickly
+map("n", "<C-s>", ":w<CR>", opts) -- Save file with Ctrl + S
+map("n", "<C-q>", ":q<CR>", opts) -- Quit file with Ctrl + Q
+map("n", "<C-a>", "ggVG", opts) -- Select all
+
+-- === Window Navigation ===
+map("n", "<C-h>", "<C-w>h", opts) -- Move to left split
+map("n", "<C-l>", "<C-w>l", opts) -- Move to right split
+map("n", "<C-j>", "<C-w>j", opts) -- Move to below split
+map("n", "<C-k>", "<C-w>k", opts) -- Move to above split
+
+-- === Buffer Navigation ===
+map("n", "<Tab>", ":bnext<CR>", opts) -- Next buffer
+map("n", "<S-Tab>", ":bprevious<CR>", opts) -- Previous buffer
+map("n", "<Leader>bd", ":bd<CR>", opts) -- Close current buffer
+
+-- === Split Window Management ===
+map("n", "<Leader>sv", ":vsplit<CR>", opts) -- Vertical split
+map("n", "<Leader>sh", ":split<CR>", opts) -- Horizontal split
+map("n", "<Leader>sx", ":close<CR>", opts) -- Close split
+
+-- === Clipboard & Text Manipulation ===
+map("v", "<Leader>y", '"+y', opts) -- Copy to system clipboard
+map("n", "<Leader>p", '"+p', opts) -- Paste from system clipboard
+map("n", "x", '"_x', opts) -- Delete character without copying to clipboard
+
+-- === Indentation ===
+map("v", "<", "<gv", opts) -- Indent left
+map("v", ">", ">gv", opts) -- Indent right
+
+-- === Searching ===
+map("n", "n", "nzzzv", opts) -- Keep search results centered
+map("n", "N", "Nzzzv", opts) -- Keep search results centered
+map("n", "J", "mzJ`z", opts) -- Join lines but keep cursor position
+map("n", "<Esc>", ":noh<CR>", opts) -- Clear search highlight
+
+-- === Terminal Mode Mappings ===
+map("t", "<Esc>", "<C-\\><C-n>", opts) -- Exit terminal mode
+map("t", "<C-h>", "<C-\\><C-n><C-w>h", opts)
+map("t", "<C-j>", "<C-\\><C-n><C-w>j", opts)
+map("t", "<C-k>", "<C-\\><C-n><C-w>k", opts)
+map("t", "<C-l>", "<C-\\><C-n><C-w>l", opts)
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,58 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+-- Set options
+local opt = vim.opt
+
+-- UI Settings
+opt.number = true -- Show line numbers
+opt.relativenumber = true -- Relative line numbers
+opt.cursorline = true -- Highlight current line
+opt.termguicolors = true -- Enable true colors
+opt.signcolumn = "yes" -- Always show sign column
+
+-- Indentation & Formatting
+opt.expandtab = true -- Convert tabs to spaces
+opt.shiftwidth = 4 -- Indentation width
+opt.tabstop = 4 -- Number of spaces per tab
+opt.smartindent = true -- Smart indentation
+opt.wrap = false -- Disable line wrapping
+
+-- Search Behavior
+opt.ignorecase = true -- Case-insensitive search
+opt.smartcase = true -- Case-sensitive if uppercase in search
+opt.hlsearch = true -- Highlight search results
+opt.incsearch = true -- Incremental search
+
+-- Performance & Responsiveness
+opt.updatetime = 300 -- Reduce delay before writing swap files
+opt.timeoutlen = 500 -- Faster key sequence recognition
+opt.ttimeoutlen = 10 -- Faster escape key timeout
+
+-- Splitting Behavior
+opt.splitbelow = true -- Split below instead of above
+opt.splitright = true -- Split right instead of left
+
+-- Clipboard Integration
+opt.clipboard = "unnamedplus" -- Use system clipboard
+
+-- Undo & Backup
+opt.undofile = true -- Persistent undo
+opt.undodir = vim.fn.stdpath("data") .. "/undo"
+
+-- Disable swap file
+opt.swapfile = false
+opt.backup = false
+
+-- Enable mouse support
+opt.mouse = "a"
+
+-- Disable line wrapping in Markdown, but enable spell check
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
