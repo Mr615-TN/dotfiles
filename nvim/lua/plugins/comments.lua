5e1ad2b6391904193854d196f208b2f9d9a6106d
Tanish Neema <tneema501@gmail.com>
2025-04-03 19:59:24 -0500

    Update: comments.lua and lazy lock

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -2,4 +2,11 @@
 
 -- You can add any setup or configurations here if you need them later.
 -- For now, leaving it empty as you mentioned no options.
-require("Comment").setup()
+return {
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup() -- Set up the plugin after it's loaded
+    end,
+  },
+}
