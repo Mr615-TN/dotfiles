122fe0cc2a2227076b7f29a3ad7638743b165c8d
Tanish Neema <tneema501@gmail.com>
2025-04-12 10:52:41 -0500

    Update: remvoed stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,2 @@
--- Load the splash screen before plugins
-local status_ok, splash = pcall(require, "config.splash")
-if status_ok and splash and splash.show_splash then
-  splash.show_splash()
-end
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
