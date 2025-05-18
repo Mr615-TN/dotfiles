0485be10c0f6b02b77dec3da23595c6c6c8032db
Tanish Neema <tneema501@gmail.com>
2025-05-18 12:05:15 -0500

    Update: lazy lock and minimap

---
+++ nvim/lua/plugins/minimap.lua
@@ -0,0 +1,20 @@
+return {
+  "Isrothy/neominimap.nvim",
+  lazy = false, -- Recommended to load immediately
+  config = function()
+    -- Recommended options for float layout
+    vim.opt.wrap = false
+    vim.opt.sidescrolloff = 36
+
+    --- Neominimap configuration
+    ---@type Neominimap.UserConfig
+    vim.g.neominimap = {
+      auto_enable = true, -- Enable the minimap globally by default
+    }
+  end,
+  keys = {
+    -- Global Minimap Controls
+    { "<leader>nm", "<cmd>Neominimap Toggle<cr>", desc = "Toggle global minimap" },
+    { "<leader>nr", "<cmd>Neominimap Refresh<cr>", desc = "Refresh global minimap" },
+  },
+}
