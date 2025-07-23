80c671c98c71c316842e272e642acd0c3f8d2157
Tanish Neema <tneema501@gmail.com>
2025-07-23 13:02:17 -0500

    Update: stuff

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -28,4 +28,45 @@ return {
     -- For providers, they need to be defined directly here.
     opts_extend = { "sources.default" },
   },
+
+  -- render-markdown.nvim configuration
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "echasnovski/mini.nvim", -- Use mini.nvim for icons
+    },
+    config = function()
+      require("render-markdown").setup({
+        -- Configuration for markdown files
+        -- You can add specific options for markdown rendering here if needed.
+
+        -- Enable completions for blink.cmp
+        completions = {
+          blink = {
+            enabled = true,
+          },
+        },
+
+        -- Basic configuration for LaTeX files
+        filetypes = {
+          latex = {
+            -- You can add specific options for LaTeX rendering here if render-markdown.nvim supports them.
+            -- For example, if it had a 'render_command' option for LaTeX:
+            -- render_command = { 'pdflatex', '-interaction=nonstopmode', '$file_name$' },
+          },
+        },
+      })
+    end,
+  },
+
+  -- mini.nvim for icons (dependency for render-markdown.nvim)
+  {
+    "echasnovski/mini.nvim",
+    version = "*", -- Use latest stable release
+    config = function()
+      -- Minimal setup for mini.nvim if you only need it for icons
+      -- You can configure specific mini modules here if you want to use more of its features.
+    end,
+  },
 }
