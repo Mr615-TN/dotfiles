50d0b1b2ecd14fa46a275558854756f5b96219d9
Tanish Neema <tneema501@gmail.com>
2025-07-31 14:48:42 -0500

    Update: stuff

--- doom/config.el
+++ doom/config.el
@@ -40,8 +40,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
+(setq org-directory "~/Documents/org/")
+(setq org-agenda-files "~/Documents/org/")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -75,6 +75,10 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 ;; Define a list of directories to be scanned for Org files
-(setq org-agenda-files (list "~/org/"
-                             "~/GitHub/DiigitalNotebook/WorkNotes/VLA/Notes"
-                             "~/projects/project-beta/"))
+(after! org-preview-html
+  ;; This command opens the HTML preview in a side window.
+  (map! :leader
+        :prefix "o"
+        :desc "Toggle live HTML preview"
+        "h" #'org-preview-html-mode)
+  )
