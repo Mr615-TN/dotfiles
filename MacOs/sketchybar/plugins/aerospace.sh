6272dfbfbb8fad570553d71bac168974d11c1523
Tanish Neema <tneema501@gmail.com>
2025-10-08 10:44:20 -0500

    Update

--- MacOs/sketchybar/plugins/aerospace.sh
+++ MacOs/sketchybar/plugins/aerospace.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
 # make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+# Get the focused workspace
+FOCUSED=$(aerospace list-workspaces --focused)
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+# The workspace ID is passed as $1
+WORKSPACE_ID="$1"
+
+if [ "$WORKSPACE_ID" = "$FOCUSED" ]; then
+    # Highlight the focused workspace with racing red
+    sketchybar --set $NAME \
+        background.drawing=on \
+        background.color=0xffcc0000 \
+        label.color=0xfffffff8
 else
-    sketchybar --set $NAME background.drawing=off
+    # Non-focused workspaces use pearl white background
+    sketchybar --set $NAME \
+        background.drawing=on \
+        background.color=0xeef5f5f5 \
+        label.color=0xff0a0a0a
 fi
