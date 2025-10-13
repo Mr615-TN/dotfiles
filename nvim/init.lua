e47200334f6132e8b339fd33ee4b61036b9eb9fb
Tanish Neema <tneema501@gmail.com>
2025-10-13 17:27:04 -0500

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,18 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("config.options")
 require("config.lazy")
+require("config.keymaps")
+require("config.autocmds")
