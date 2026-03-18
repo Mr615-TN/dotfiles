9aae7c68bb06980242c1f4eefcab584ab8b9029e
Tanish Neema <tneema501@gmail.com>
2026-03-18 00:18:50 -0500

    Update

--- nvim/lua/plugins/twilight.lua
+++ nvim/lua/plugins/twilight.lua
@@ -1,8 +1,12 @@
 return {
-  "folke/twilight.nvim",
-  opts = {
-    -- You can customize your settings here. 
-    -- For example:
-    -- dimming = { alpha = 0.25 },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      plugins = {
+        twilight = { enabled = true }, -- This is the "auto-toggle" magic
+      },
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
   },
+  { "folke/twilight.nvim", opts = {} },
 }
